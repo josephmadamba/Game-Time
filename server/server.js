8118caa3bb6c30d9a091085b69c4db6c9c88ca85
Joseph Madamba <jpmadamba@gmail.com>
2019-03-26 20:30:24 -0500

    getting heroku to work locally

--- server/server.js
+++ server/server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const path = require('path');
 const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname, 'client/build')));
-
+app.use(express.static(path.join(__dirname, '..', 'client/build')));
+ console.log(__dirname)
 
 
 app.use(require('./routes/userRoute'))
