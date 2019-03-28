4702039babcdd46032cd80ff9bded6813f2ba58f
KimjManansala <kimjmanansala@gmail.com>
2019-03-28 12:28:17 -0500

    created the beginnig of the about page

---
+++ client/src/components/AboutFunctions.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+export const purpose = () => (
+  <React.Fragment>
+    <p>
+      The purpose of PickUpe.com is to help people find and play pickup
+      basketball games. We wanted to create a place where people looking for
+      games could find accurate, up-to-date information and game coordinators
+      could easily publish that information. We also wanted to create a way for
+      the communities around a game to stay connected and keep a running dialog
+      about their game.
+    </p>
+    <p>
+      Our Future goal is to create a profile system to display competitive level
+      and ranking of the user.
+    </p>
+  </React.Fragment>
+);
+
+export const developers = () => (
+  <p>
+  <a href="https://www.linkedin.com/in/kim-jasper-manansala/" target="_blank" >KimJ Manansala </a>, <a href="https://www.linkedin.com/in/aarandela/" target="_blank">Audy Arandela, </a> and <a href='https://www.linkedin.com/in/ACoAACkFypMBesXBITQHD5wHwj98F_X4F_OTVJ8/' target="_blank">Joseph Madamba</a> are the developers from
+    Houston, TX and New York, NY. Please reach to us for anything you want us to add to Pickup!
+  </p>
+);
