4064a8c780d3fc43beb44463f5e1e39015b2a964
KimjManansala <kimjmanansala@gmail.com>
2019-03-28 17:15:58 -0500

    Fixed routes and server routes

--- server/server.js
+++ server/server.js
@@ -10,14 +10,22 @@ app.use(express.static(path.join(__dirname, '..', 'client/build')));
  console.log(__dirname)
 
 
-app.use(require('./routes/userRoute'))
-app.use(require('./routes/gamesRoutes'))
+ app.use(require('./routes/userRoute'))
+ app.use(require('./routes/gamesRoutes'))
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+    res.sendFile(path.join(__dirname, '..' ,'client/build/index.html'));
   });
   
   
+// Handles unknown routes
+app.use((req, res, next) => {
+  const err = new Error("Not Found");
+  err.status = 404;
+  console.log(err);
+  res.send("404: Page Not Found");
+  next(err);
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
