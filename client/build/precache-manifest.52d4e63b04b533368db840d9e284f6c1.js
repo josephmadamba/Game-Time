8a5c75bbd14d49d6383e9e6ad964fc7c895cc679
KimjManansala <kimjmanansala@gmail.com>
2019-03-29 19:11:03 -0500

    buidl

---
+++ client/build/precache-manifest.52d4e63b04b533368db840d9e284f6c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "cf8aeb26036ce2006bd54ec8ff934c7a",
+    "url": "/static/media/basketball.cf8aeb26.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "bd4095aa25291710e0d6",
+    "url": "/static/js/main.5115c643.chunk.js"
+  },
+  {
+    "revision": "f573777981ff831dd04a",
+    "url": "/static/js/2.f418104c.chunk.js"
+  },
+  {
+    "revision": "bd4095aa25291710e0d6",
+    "url": "/static/css/main.8dc6d3b1.chunk.css"
+  },
+  {
+    "revision": "f573777981ff831dd04a",
+    "url": "/static/css/2.924e7f46.chunk.css"
+  },
+  {
+    "revision": "d926b697c34b54cd38c908bcea1a42bb",
+    "url": "/index.html"
+  }
+];
