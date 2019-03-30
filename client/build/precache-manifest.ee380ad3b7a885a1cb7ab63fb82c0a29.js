85aa006f37b5926d15b7c55daa47ae08397ef994
KimjManansala <kimjmanansala@gmail.com>
2019-03-30 09:26:14 -0500

    Updated build

---
+++ client/build/precache-manifest.ee380ad3b7a885a1cb7ab63fb82c0a29.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "104b828b31b3ad727849fad80cc3ca1d",
+    "url": "/static/media/shot.104b828b.mp4"
+  },
+  {
+    "revision": "cf8aeb26036ce2006bd54ec8ff934c7a",
+    "url": "/static/media/basketball.cf8aeb26.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "444b1ae643bdbf0c6078",
+    "url": "/static/js/main.b2a114f2.chunk.js"
+  },
+  {
+    "revision": "e3b6c6568820185a69cf",
+    "url": "/static/js/2.3bdc7c0d.chunk.js"
+  },
+  {
+    "revision": "444b1ae643bdbf0c6078",
+    "url": "/static/css/main.ae6db5c2.chunk.css"
+  },
+  {
+    "revision": "e3b6c6568820185a69cf",
+    "url": "/static/css/2.924e7f46.chunk.css"
+  },
+  {
+    "revision": "0fee73e1d971286082f795dfac2c89a6",
+    "url": "/index.html"
+  }
+];
