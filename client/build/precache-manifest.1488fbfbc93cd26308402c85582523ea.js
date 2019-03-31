63b2918e2b60add54cd88df81a14fd3c73907a1d
Audy Arandela <audyarandela@gmail.com>
2019-03-30 23:08:18 -0500

    landing page up

---
+++ client/build/precache-manifest.1488fbfbc93cd26308402c85582523ea.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "658b3e8f4d7d6fdbb8c4",
+    "url": "/static/css/main.2e188fce.chunk.css"
+  },
+  {
+    "revision": "658b3e8f4d7d6fdbb8c4",
+    "url": "/static/js/main.032401f7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "51c8235b17a897291748",
+    "url": "/static/css/2.42066a9c.chunk.css"
+  },
+  {
+    "revision": "51c8235b17a897291748",
+    "url": "/static/js/2.a9a89256.chunk.js"
+  },
+  {
+    "revision": "e0e5c67169c5f8ae328495258c110817",
+    "url": "/static/media/shot.e0e5c671.gif"
+  },
+  {
+    "revision": "c661c7be9c19b2c41fd6b06ca4ca7fc5",
+    "url": "/static/media/Got.c661c7be.png"
+  },
+  {
+    "revision": "53513b6903d5b8c026c9ebee1b5c6b3e",
+    "url": "/static/media/game.53513b69.png"
+  },
+  {
+    "revision": "068baedb4c5e05a41ac69498ec1aea6e",
+    "url": "/static/media/createbutton.068baedb.png"
+  },
+  {
+    "revision": "8608d679b34de83748f42d020bbf242d",
+    "url": "/static/media/Or.8608d679.png"
+  },
+  {
+    "revision": "055e0c69ada1ed11c94466779fedc219",
+    "url": "/static/media/joinbutton.055e0c69.png"
+  },
+  {
+    "revision": "7853a7f96fed0eb86c20a09a97f15691",
+    "url": "/static/media/a.7853a7f9.png"
+  },
+  {
+    "revision": "ee266b3cdb57f47e4a901f29b8658e8f",
+    "url": "/static/media/gamereg.ee266b3c.png"
+  },
+  {
+    "revision": "82cbd876e8247526e378f723ff8a1cdb",
+    "url": "/static/media/today.82cbd876.png"
+  },
+  {
+    "revision": "cf8aeb26036ce2006bd54ec8ff934c7a",
+    "url": "/static/media/basketball.cf8aeb26.jpg"
+  },
+  {
+    "revision": "7763a9889990e20c6f07ceafa67bcd49",
+    "url": "/index.html"
+  }
+];
--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -12,16 +12,14 @@ import A from '../img/a.png'
 import GAMEREG from '../img/gamereg.png'
 import TODAY from '../img/today.png'
 
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import Tooltip from '@material-ui/core/Tooltip';
-
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
 
 const styles = theme => ({
   root: {
     width: '100%',
     maxWidth: 500,
-    height: '100%', 
+    height: '100%',
     maxHeight: 500
   },
   video: {
@@ -33,41 +31,41 @@ const styles = theme => ({
     background: 'scroll',
     backgroundPosition: 'center',
     backgroundRepeat: 'no-repeat',
-    backgroundSize: 'cover',
+    backgroundSize: 'cover'
   },
   got: {
-    width: "67%",
-    height: "50%",
+    width: '67%',
+    height: '50%'
   },
   game: {
-    width: "100%",
-    height: "50%"
+    width: '100%',
+    height: '50%'
   },
   create: {
-    width: "100%",
-    height: "50%",
+    width: '100%',
+    height: '50%',
     margin: 'theme.spacing.unit'
   },
   or: {
-    width: "10%",
-    height: "50%",
-    align: "right"
+    width: '10%',
+    height: '50%',
+    align: 'right'
   },
   join: {
-    width: "100%",
-    height: "50%"
+    width: '100%',
+    height: '50%'
   },
   a: {
-    width: "5%",
-    height: "50%"
+    width: '5%',
+    height: '50%'
   },
   gamereg: {
-    width: "20%",
-    height: "50%"
+    width: '20%',
+    height: '50%'
   },
   today: {
-    width: "24%",
-    height: "50%"
+    width: '24%',
+    height: '50%'
   }
 })
 
@@ -82,54 +80,42 @@ class Landing extends Component {
 
       <div className={classes.root}>
         <div className={classes.video} />
-   
+
         <Typography align='center'>
-          {/* &nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp; */}
 
           <img className={`${classes.got} wow bounceInLeft`} data-wow-duration='2.1s' data-wow-delay='2s' src={GOT} alt='' />
         </Typography>
 
         <Typography align='center'>
-         {/* &nbsp; &nbsp;&nbsp; &nbsp;&nbsp; */}
           <img className={`${classes.game} wow bounceInUp`} data-wow-duration='2s' data-wow-delay='2.2s' src={GAME} alt='' />
         </Typography>
 
-        <br/> <br/> <br/> <br/>  <br/> <br/>  <br/> <br/> 
+        <br /> <br /> <br /> <br />  <br /> <br />  <br /> <br />
 
-        <Typography align='center'> 
-        {/* &nbsp; &nbsp;&nbsp; &nbsp;&nbsp; */}
+        <Typography align='center'>
+          <Button href='/user/account'>
+            <img className={`${classes.create} wow bounceInDown`} data-wow-duration='2s' data-wow-delay='6s' src={CREATE} alt='' />
+          </Button>
 
-          
-            <Button href='/user/account'>
-              <img className={`${classes.create} wow bounceInDown`} data-wow-duration='2s' data-wow-delay='6s' src={CREATE} alt='' /> 
-            </Button>
-       
-           <br/>
+          <br />
 
-          {/* &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;  */}
-
-          <img className={`${classes.or} wow bounceInRight`} data-wow-duration='2s' data-wow-delay='6.1s'  src={OR} alt='' /> <br/>
-
-          {/* &nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp;  */}
+          <img className={`${classes.or} wow bounceInRight`} data-wow-duration='2s' data-wow-delay='6.1s' src={OR} alt='' /> <br />
           <Button href='/user/account'>
             <img className={`${classes.join} wow bounceInLeft`} data-wow-duration='2s' data-wow-delay='6.2s' src={JOIN} alt='' />
           </Button>
         </Typography>
-       
+
         <Typography align='center'>
-          {/* &nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp;  */}
+          <img className={`${classes.a} wow bounceInLeft`} data-wow-duration='2s' data-wow-delay='6.3s' src={A} alt='' />
 
-          <img className={`${classes.a} wow bounceInLeft`} data-wow-duration='2s' data-wow-delay='6.3s' src={A} alt='' /> &nbsp;&nbsp;
+          &nbsp;&nbsp;&nbsp;
 
-          &nbsp; 
+          <img className={`${classes.gamereg} wow bounceInUp`} data-wow-duration='2s' data-wow-delay='6.4s' src={GAMEREG} alt='' />
 
-          <img className={`${classes.gamereg} wow bounceInUp`} data-wow-duration='2s' data-wow-delay='6.4s' src={GAMEREG} alt='' /> &nbsp; &nbsp; 
-
-          &nbsp; 
+          &nbsp; &nbsp; &nbsp;
 
           <img className={`${classes.today} wow bounceInRight`} data-wow-duration='2s' data-wow-delay='6.5s' src={TODAY} alt='' />
         </Typography>
-
       </div>
     )
   }
